8dd006bc9a6468a54d33b39effc987433b331308
passy4ucj <ojinnnakapascal@gmail.com>
2025-03-26 09:02:54 +0100

    register - get users services

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,3 +1,4 @@
+import e from "express";
 import { prisma } from "../client";
 import { User } from "@prisma/client";
 
@@ -13,4 +14,39 @@ export type UserSettings = Pick<
 
 // excluded password types
 export type ReturnedUser = Partial<Pick<User, "password">> &
-Omit<User, "password">;
+    Omit<User, "password">;
+
+export const registerUser = async (data: UserAccount): Promise<ReturnedUser> => {
+
+    const user: ReturnedUser = await prisma.user.create({
+        data: { ...data },
+    });
+    delete user.password;
+
+    return user;
+};
+
+export const findUserByIdService = async (userId: string): Promise<ReturnedUser | null> => {
+    
+    return await prisma.user.findUnique({ 
+        where: { id: userId },
+    });
+};
+
+export const findUser = async (
+    email: string
+): Promise<ReturnedUser | null> => {
+    return await prisma.user.findUnique({ where: { email } });
+};
+
+export const getAllUserService = async () => {
+    return await prisma.user.findMany({
+        select: {
+        id: true,
+        fullName: true,
+        phoneNumber: true,
+        email: true,
+        createdAt: true,
+        },
+    });
+};
